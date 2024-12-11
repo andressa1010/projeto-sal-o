36fb9be6f37a17147e8c12ff04f89dd05f8c5d2c
andressa1010 <andressa.ns10@gmail.com>
2024-12-11 09:28:42 -0300

    alterar-styled

--- src/Styles/styled.js
+++ src/Styles/styled.js
@@ -51,14 +51,14 @@ export const ContainerDiv = styled.div`
     width: 400px;
   }
 
-  @media(max-width: 500px){
-     img{
-      width: 300px;
-     }
-     p{
+  @media (max-width: 500px) {
+    img {
+      width: 200px;
+    }
+    p {
       font-size: 16px;
       margin: 10px;
-     }
+    }
   }
 `;
 
@@ -82,14 +82,14 @@ export const ContainerConteudo = styled.div`
     border-radius: 40px;
   }
 
-  @media(max-width: 500px){
-     img{
-      width: 300px;
-     }
-     p{
+  @media (max-width: 500px) {
+    img {
+      width: 200px;
+    }
+    p {
       font-size: 16px;
       margin: 10px;
-     }
+    }
   }
 `;
 
@@ -105,13 +105,13 @@ export const DivTitulo = styled.div`
     color: goldenrod;
   }
 
-  @media(max-width: 500px){
-    h2{
+  @media (max-width: 500px) {
+    h2 {
       font-size: 25px;
-    font-family: "Pacifico", serif;
-    font-weight: 400;
-    font-style: normal;
-    color: goldenrod;
+      font-family: "Pacifico", serif;
+      font-weight: 400;
+      font-style: normal;
+      color: goldenrod;
     }
   }
 `;
@@ -122,7 +122,7 @@ export const DivFotos = styled.div`
   flex-wrap: wrap;
   padding: 10px;
 
-  h2{
+  h2 {
     color: goldenrod;
   }
 
@@ -139,10 +139,20 @@ export const DivFotos = styled.div`
     border-radius: 40px;
   }
 
-  @media(max-width: 500px){
-        .div-fotos{
-          width: 200px;
-        }
+  @media (max-width: 500px) {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+
+    .div-fotos {
+      margin: 10px;
+      padding: 10px;
+      width: calc(
+        50% - 20px
+      ); /* Ajusta para 50% do espaço disponível, com margem */
+      text-align: center;
+      box-sizing: border-box;
+    }
   }
 `;
 
@@ -151,178 +161,201 @@ export const ContainerDescricao = styled.div`
   justify-content: center;
   align-items: center;
 
-  p{
+  p {
     font-size: 26px;
     max-width: 600px;
     color: goldenrod;
   }
 
-  img{
+  img {
     width: 400px;
     border-radius: 40px;
   }
 
-  @media(max-width: 500px){
-     img{
-      width: 300px;
-     }
-     p{
-      font-size: 16px;
-      margin: 10px;
-     }
-  }
-`
-
-export const ContainerServicos= styled.div`
-    display: flex;
-    justify-content: space-between;
-    flex-wrap: wrap;
-    padding: 10px;
-
-    h2{
-        color: goldenrod;
-    }
-
-    .produto{
-        margin: 10px;
-        padding: 10px;
-        width: 300px;
-        text-align: center;
-    }
-
-    video{
-        max-width: 100%;
-        height: auto;
-        border-radius: 40px;
-    }
-
-    @media(max-width: 500px){
-      .produto{
-        width: 200px;
-      }
-  }
-
-`
-
-export const ContainerBanner= styled.div`
-    display: flex;
-    justify-content: space-between;
-    flex-wrap: wrap;
-    padding: 10px;
-
-    img{
-        width: 300px;
-        border-radius: 40px;
-    }
-
-  @media(max-width: 500px){
-    img{
+  @media (max-width: 500px) {
+    img {
       width: 200px;
     }
+    p {
+      font-size: 16px;
+      margin: 10px;
+    }
+  }
+`;
+
+export const ContainerServicos = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  padding: 10px;
+
+  h2 {
+    color: goldenrod;
   }
 
-`
-
-export const ContainerTrabalhos= styled.div`
-     display: flex;
-     justify-content: center;
-     align-items: center;
-     gap: 10px;
-
-     p{
-        font-size: 26px;
-        max-width: 600px;
-        color: goldenrod;
-     }
-
-     video{
-        width: 300px;
-        border-radius: 40px;
-     }
-
-@media(max-width: 500px){
-  p{
-    font-size: 20px;
+  .produto {
+    margin: 10px;
+    padding: 10px;
+    width: 300px;
+    text-align: center;
   }
-  video{
-    width: 200px;
+
+  video {
+    max-width: 100%;
+    height: auto;
+    border-radius: 40px;
   }
-}
-`
 
-export const ContainerMotivos = styled.div`
-   display: flex;
-   justify-content: center;
-   align-items: center;
+  @media (max-width: 500px) {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
 
-   p{
+    .produto {
+      margin: 10px;
+      padding: 10px;
+      width: calc(
+        50% - 20px
+      ); /* Ajusta para 50% do espaço disponível, com margem */
+      text-align: center;
+      box-sizing: border-box;
+    }
+  }
+`;
+
+export const ContainerBanner = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  padding: 10px;
+
+  .container-img{
+    margin: 10px;
+    padding: 10px;
+    width: 300px;
+    text-align: center;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+    border-radius: 40px;
+  }
+
+  @media (max-width: 500px) {
+      display: flex;
+      justify-content: center;
+      flex-wrap: wrap;
+
+      .container-img{
+        margin: 10px;
+      padding: 10px;
+      width: calc(
+        50% - 20px
+      ); /* Ajusta para 50% do espaço disponível, com margem */
+      text-align: center;
+      box-sizing: border-box;
+      }
+  }
+`;
+
+export const ContainerTrabalhos = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+
+  p {
     font-size: 26px;
     max-width: 600px;
     color: goldenrod;
-   }
+  }
 
-   img{
+  video {
+    width: 300px;
+    border-radius: 40px;
+  }
+
+  @media (max-width: 500px) {
+    p {
+      font-size: 20px;
+    }
+    video {
+      width: 200px;
+    }
+  }
+`;
+
+export const ContainerMotivos = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  p {
+    font-size: 26px;
+    max-width: 600px;
+    color: goldenrod;
+  }
+
+  img {
     width: 400px;
-   }
+  }
 
-   @media(max-width: 500px){
-     img{
-      width: 300px;
-     }
-     p{
+  @media (max-width: 500px) {
+    img {
+      width: 200px;
+    }
+    p {
       font-size: 16px;
       margin: 10px;
-     }
+    }
   }
-
-`
+`;
 
 export const ContainerTratamentos = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
-    p{
-        font-size: 26px;
-        color: goldenrod;
-        max-width: 600px;
-    }
-
-    video{
-        width: 300px;
-        border-radius: 40px;
-    }
-
-    @media(max-width: 500px){
-  p{
-    font-size: 20px;
+  p {
+    font-size: 26px;
+    color: goldenrod;
+    max-width: 600px;
   }
-  video{
-    width: 200px;
+
+  video {
+    width: 300px;
+    border-radius: 40px;
   }
-}
-`
+
+  @media (max-width: 500px) {
+    p {
+      font-size: 20px;
+    }
+    video {
+      width: 200px;
+    }
+  }
+`;
 
 export const MapsLocalizacao = styled.div`
-    display: flex;
-    justify-content: center;
+  display: flex;
+  justify-content: center;
 
-    .maps{
-        width: 600px;
-        
-    }
+  .maps {
+    width: 600px;
+  }
 
-    iframe{
-        border-radius: 40px;
-    }
-`
+  iframe {
+    border-radius: 40px;
+  }
+`;
 
 export const ContainerZap = styled.div`
-   display: flex;
-   justify-content: end;
+  display: flex;
+  justify-content: end;
 
-   img{
+  img {
     height: 60px;
-   }
-
-`
+  }
+`;
